d9c98af68cffb075c052df78cfab3389a42c34d1
shanu <shanu@BOOK-4QTRLAP57K>
2024-12-02 17:07:30 +0530

    Commited maevn projetc

---
+++ SampleMavenProject/src/main/java/mavenpackk/MavenSamplllcls.java
@@ -0,0 +1,17 @@
+package mavenpackk;
+
+public class MavenSamplllcls {
+
+	public static void main(String[] args) {
+
+		int a = -10;
+		
+		if (a > 0) {
+			System.out.println("positive");
+		} else if (a < 0) {
+			System.out.println("negative");
+		}
+
+	}
+
+}
